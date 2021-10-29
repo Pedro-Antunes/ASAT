464e8c31ae1dc001056c6f5a865732b08ee27245
Pedro-Antunes <48651144+Pedro-Antunes@users.noreply.github.com>
2021-10-29 15:15:06 +0100

    .

--- individuo.py
+++ individuo.py
@@ -3,6 +3,7 @@ class Individuo:
     def __init__(self, id):
         self._id = id
         self._val = Valoracao()
+        self._coef = 0
         self._mem = []
         self._actv = Valoracao()
         self._PrMut = 0.5
@@ -10,11 +11,17 @@ class Individuo:
     def setVal(self, val):
         self._val = val
 
+    def setCoef(self, value):
+        self._coef = value
+
     def getId(self):
         return self._id
     
     def getVal(self):
         return self._val
+
+    def getCoef(self):
+        return self._coef
 
     def getPrMut(self):
         return self._PrMut
