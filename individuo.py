d510deb28f65a5526b6d22af9a2e91f0e254bcc8
Pedro-Antunes <48651144+Pedro-Antunes@users.noreply.github.com>
2021-10-29 14:43:13 +0100

    .

--- individuo.py
+++ individuo.py
@@ -1,11 +1,14 @@
 class Individuo:
 
-    def __init__(self, identifier):
-        self._id = identifier
+    def __init__(self, id):
+        self._id = id
         self._val = Valoracao()
         self._mem = []
         self._actv = Valoracao()
         self._PrMut = 0.5
+
+    def setVal(self, val):
+        self._val = val
 
     def getId(self):
         return self._id
@@ -16,8 +19,11 @@ class Individuo:
     def getPrMut(self):
         return self._PrMut
 
-    def memorise(self, x):
+    def memorize(self, x):
         self._mem.append(x)
 
+    def valCount(self):
+        return len(self._mem)
+
     def uniqueValCount(self):
-        return len(set(self.mem))
+        pass
