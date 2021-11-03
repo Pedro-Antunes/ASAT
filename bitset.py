323ba9ca207e2038bfa838026a38bf8acece33ce
Pedro-Antunes <48651144+Pedro-Antunes@users.noreply.github.com>
2021-11-03 11:31:50 +0000

    .

--- bitset.py
+++ bitset.py
@@ -1,9 +1,11 @@
+import math
+
+
 class Bitset:
     
     def __init__(self):
         self.data = 0
-        self.trueBits = 0
-    
+
     def set(self, i):
         self.data |= 1<<i
         
@@ -13,10 +15,13 @@ class Bitset:
     def flip(self, i):
         self.data ^= 1<<i
         
-    def get(self, i):
-        # TA A DAR BUG AQUI PLS HELP
-        print(type(self.data), type(1 << i))
-        return bool(self.data & (1 << i))
+    def test(self, i):
+        return bool(self.data & 1<<i)
 
     def count(self):
-        pass
+        c = 0
+        bits = self.data
+        while bits:
+            c += 1
+            bits -= bits & -bits
+        return c
