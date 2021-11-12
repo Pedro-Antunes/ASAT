b9ffe1e260732b0a11faaa2ba1adc8b7e113708c
HalfQuark <93316457+RuiZhuWang@users.noreply.github.com>
2021-11-12 21:44:07 +0000

    .

--- cap_alt.py
+++ cap_alt.py
@@ -1,13 +1,34 @@
 class CAP:
 
     def __init__(self):
-        self._eventHeap = Heap(lambda evt1, evt2: evt1.getTime() > evt2.getTime())
+        self._eventMap = {}
 
-    def add(self, evt):
-        self._eventHeap.push(evt)
+    def add(self, evento):
+        if evento.getKind() not in self._eventMap:
+            self._eventMap[evento.getKind()] = []
+        kindList = self._eventMap[evento.getKind()]
+        left = 0
+        right = len(kindList)
+        while left < right:
+            mid = (left + right) // 2
+            if evento.getTime() > kindList[mid].getTime():
+                right = mid
+            else:
+                left = mid + 1
+        kindList.insert(left, evento)
 
     def next(self):
-        return self._eventHeap.top()
+        assert len(self._eventMap) > 0
+        nextEvent = None
+        for lst in self._eventMap.values():
+            if nextEvent is None:
+                nextEvent = lst[-1]
+            elif lst[-1].getTime() < nextEvent.getTime():
+                nextEvent = lst[-1]
+        return nextEvent
 
     def remove(self):
-        self._eventHeap.pop()
+        nextEvent = self.next()
+        self._eventMap[nextEvent.getKind()].pop()
+        if self._eventMap[nextEvent.getKind()] == []:
+            self._eventMap.pop(nextEvent.getKind())
